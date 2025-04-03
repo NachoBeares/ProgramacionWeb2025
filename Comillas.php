7c2ccd87a69cb574f0ed057b0b2ca9ed48e16826
IGNACIO BEARES PEREZ <57132711@ceibal.edu.uy>
2025-04-03 14:01:01 -0300

    Primer Commit

---
+++ Comillas.php
@@ -0,0 +1,10 @@
+<?php
+$emisora 'Radio Disney' ;
+echo "Me gusta escuchar $emisora" ;
+
+/* 
+Usa comillas simples cuando no necesitas interpolar variables o cuando quieres que el texto se trate de forma literal.
+Usa comillas dobles cuando necesitas incluir variables o caracteres especiales que deben ser interpretados.
+Ambas opciones son válidas, y la elección entre una u otra depende del contexto y de tus necesidades específicas en el código.
+*/
+?>
