46b62b770617457ffeeddeaa6facabff61e1b314
NachoB2 <nachobeares3@gmail.com>
2025-05-08 15:09:47 -0300

    Promocion Terminado

--- Promocion.php
+++ Promocion.php
@@ -1,14 +1,13 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $Edad = floatval($_POST['Edad']);
-    echo " Su edad es apropiada para la promoción";
-    if ($Edad > 19) {
-        echo " No puede participar en la promoción";
-        if ($Edad < 11 ) {
-            echo " No puede participar en la promoción";
-        
+    $edad = intval($_POST["edad"]);
+    if ($edad >= 11 && $edad <= 19) {
+       echo " <p> Puede participar en la promoción</p>";
     } else {
-        echo " Puede participar en la promoción";
+        echo " <p> No puede participar en la promoción </p>";
     }
 }else{
     echo "Error en la solicitud";
+}   
+
+?>
