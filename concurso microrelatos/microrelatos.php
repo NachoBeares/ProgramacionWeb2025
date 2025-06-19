da9e6a2537de7689745ef06e704be3a5333e9b15
NachoBeares <nachobeares3@gmail.com>
2025-06-19 18:57:40 -0300

    mircrorelatos terminado

--- concurso microrelatos/microrelatos.php
+++ concurso microrelatos/microrelatos.php
@@ -1,17 +1,23 @@
 <?php 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $cantPalabras = $_POST['relato'];
+
     $palabras = explode(' ', $cantPalabras);
-    
-    if ($palabras[0] >= 5) {
-        echo "El relato supera las palabras";
+    $cantidadPalabras = count($palabras);
+
+    $cantidadCaracteres = strlen($cantPalabras);
+
+
+    if ($cantidadPalabras > 100 && $cantidadCaracteres > 700) {
+        echo "Rechazado por tener más de 100 palabras y más de 700 caracteres.";
+    } elseif ($cantidadPalabras > 100) {
+        echo "Rechazado por tener más de 100 palabras.";
+    } elseif ($cantidadCaracteres > 700) {
+        echo "Rechazado por tener más de 700 caracteres.";
     } else {
-        echo "El relato ha sido enviado";
+        echo "Aceptado el relato cumple con las reglas.";
     }
-
-
 } else {
-    echo "Hubo un error";
+    echo "Error no se envió el formulario correctamente.";
 }
-
 ?>
